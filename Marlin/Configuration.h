c2399f8fb1b6f153c73436d23e140f97fb87a155
AnnabellaRenee87 <28731732+AnnabellaRenee87@users.noreply.github.com>
2020-10-19 02:03:54 -0400

    Making homing faster

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1389,8 +1389,8 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_XY (20*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_XY (40*60)
+#define HOMING_FEEDRATE_Z  (40*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
